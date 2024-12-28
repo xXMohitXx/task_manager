8d1db5f76d0f98572d72094039fdfbefe4669641
Mohit Manglani <68803303+xXMohitXx@users.noreply.github.com>
2024-12-28 11:50:29 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost:3306";
+$username = "admin";
+$password = "123";
+$db_name = "task_manager";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$db_name", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+?>
